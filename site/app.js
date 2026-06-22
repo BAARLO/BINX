8762909da5449c86a97841b96daa5baab37ea8a2
BAARLO <roland.baarlo@gmail.com>
2026-06-22 09:51:55 +0200

    Update app.js

--- site/app.js
+++ site/app.js
@@ -47,7 +47,7 @@ function renderStand() {
 
 function renderMatches() {
   const hiddenStart = '2026-06-11';
-  const hiddenEnd = '2026-06-19';
+  const hiddenEnd = '2026-06-21';
   const visibleMatches = (data.matches || []).filter(m => {
     const date = m.datum_iso || '';
     return !(date >= hiddenStart && date <= hiddenEnd);
